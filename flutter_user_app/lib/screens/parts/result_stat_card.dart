7628fb6b53b15f2994574fea8120b45b262ceae4
candy <iamcandy0515@gmail.com>
2026-03-24 13:00:33 +0900

    fix: resolve import path error in result_stat_card.dart

--- flutter_user_app/lib/screens/parts/result_stat_card.dart
+++ flutter_user_app/lib/screens/parts/result_stat_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../core/design_system.dart';
+import '../../core/design_system.dart';
 
 class ResultStatCard extends StatelessWidget {
   final int solvedCount;
